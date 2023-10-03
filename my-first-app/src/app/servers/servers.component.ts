6088a3779b9ce998b520ef948e35c27c45b99c45
james <jameswilcox.98@gmail.com>
2023-10-03 07:48:20 -0300

    start assignment 1 module 1

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template:  `
+    <p>servers component works</p>
+    <app-server></app-server>
+    <app-server></app-server>`,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+
+}
